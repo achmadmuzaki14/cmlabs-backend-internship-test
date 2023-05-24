657d7d30b317b7ac641e93cfaec387fe46ba0260
Zaki <achmadikazum91@gmail.com>
2023-05-24 14:30:07 +0700

    FizzBuzz test revision

--- fizzbuzz-test.php
+++ fizzbuzz-test.php
@@ -3,11 +3,11 @@
 for ($i = 1; $i <= 100; $i++) {
     // jika kelipatan 3 sekaligus kelipatan 5 (KPK 15)
     if ($i % 15 == 0) {
-        echo $i . " - " . "FizzBuzz\n";
+        echo "FizzBuzz\n";
     } elseif ($i % 3 == 0) {
-        echo $i . " - " . "Fizz\n";
+        echo "Fizz\n";
     } elseif ($i % 5 == 0) {
-        echo $i . " - " . "Buzz\n";
+        echo "Buzz\n";
     } else {
         echo $i . "\n";
     }
